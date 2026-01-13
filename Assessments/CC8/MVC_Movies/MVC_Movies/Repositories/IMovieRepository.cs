3aca3b1543e2414625e9cabb8c5549b931bc0c4c
Keerthi Bapanapalli <keerthib@infinite.com>
2026-01-13 13:03:02 +0530

    Infinite Training

---
+++ Assessments/CC8/MVC_Movies/MVC_Movies/Repositories/IMovieRepository.cs
@@ -0,0 +1,24 @@
+﻿using MVC_Movies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Movies.Repositories
+{
+    public interface IMovieRepository
+    {
+        IEnumerable<Movie> GetAll();
+        Movie GetById(int id);
+        void Insert(Movie entity);
+        void Update(Movie entity);
+        void Delete(int id);
+
+        IEnumerable<Movie> GetByYear(int year);
+        IEnumerable<Movie> GetByDirector(string directorName);
+
+        void Save();
+    }
+
+}
